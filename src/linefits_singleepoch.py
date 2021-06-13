46c81f0e58a89f30cbe884431cb5a58ab02effca
rterrien <rterrien@gmail.com>
2021-06-12 20:10:24 -0500

    bugfix

--- src/linefits_singleepoch.py
+++ src/linefits_singleepoch.py
@@ -1,5 +1,5 @@
 #/usr/bin/env python
-import hpfspec2.wavecalLib as wavecalLib
+from hpfspec2 import wavecalLib
 import sys
 import os
 import numpy as np
@@ -8,7 +8,6 @@ try:  # works in py2/3
     import ConfigParser
 except ImportError:
     import configparser as ConfigParser
-from PRVcommon.libs.functions import create_configdict_from_file
 from collections import OrderedDict
 import logging
 import scipy.optimize
@@ -44,6 +43,74 @@ def parse_args(raw_args=None):
                         help="Point to list of files to do line fits on, supercede L1 folder in config file")
     args = parser.parse_args(raw_args)
     return(args)
+
+def parse_str_to_types(string):
+    """ Converts string to different object types they represent.
+    Supported formats: True,Flase,None,int,float,list,tuple"""
+    if string == 'True':
+        return True
+    elif string == 'False':
+        return False
+    elif string == 'None':
+        return None
+    elif string.lstrip('-+ ').isdigit():
+        return int(string)
+    elif string == '':
+        return ''
+    elif (string[0] in '[(') and (string[-1] in ')]'): # Recursively parse a list/tuple into a list
+        if len(string.strip('()[]')) == 0:
+            return []
+        else:
+            return [parse_str_to_types(s) for s in string.strip('()[]').split(',')]
+    else:
+        try:
+            return float(string)
+        except ValueError:
+            return string
+        
+
+def create_configdict_from_file(configFilename,listOfConfigSections=None,flattenSections=True):
+    """ Returns a configuration object as a dictionary by loading the config file.
+        Values in the config files are parsed appropriately to python objects.
+    Parameters
+    ----------
+    configFilename : str
+                    File name of the config file to load
+    listOfConfigSections : list (default:None)
+                    Only the sections in the listOfConfigSections will be loaded to the dictionary.
+                    if listOfConfigSections is None (default), all the sections in config file will be loaded.
+    flattenSections: (bool, default=True)
+                    True: Flattens the sections in the config file into a single level Config dictionary.
+                    False: will return a dictionary of dictionaries for each section.
+    Returns
+    -------
+    configDictionary : dictionary
+                    if `flattenSections` is True (default): Flattened {key:value,..} dictionary is returned
+                    else: A dictionary of dictionary is returned {Section:{key:Value},..}
+    """
+    configLoader = ConfigParser.ConfigParser()
+    configLoader.optionxform = str  # preserve the Case sensitivity of keys
+    with open(configFilename) as cfgFile:
+        configLoader.read_file(cfgFile)
+
+    # Create a Config Dictionary
+    config = {}
+    if isinstance(listOfConfigSections,str): # Convert the single string to a list
+        listOfConfigSections = [listOfConfigSections]
+    elif listOfConfigSections is None:
+        listOfConfigSections = configLoader.sections()
+
+    for configSection in listOfConfigSections:
+        if flattenSections:  # Flatten the sections
+            for key,value in configLoader.items(configSection):
+                config[key] = parse_str_to_types(value)
+        else:
+            subConfig = {}
+            for key,value in configLoader.items(configSection):
+                subConfig[key] = parse_str_to_types(value)
+            config[configSection] = subConfig
+    return config
+
 
 def pool_measure_centroids_order(cmdset):
     if 'return_full' in cmdset.keys():
